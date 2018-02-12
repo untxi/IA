196a4eb58c53f107692b5ab9648acc2831376dd7
Sammy Arburola <samarburola@gmail.com>
2018-02-12 15:33:57 -0600

    mind maps

---
+++ mapas mentales/2-Inteligencia Artificial Agentes Inteligentes.mm
@@ -0,0 +1,256 @@
+
+<map version="0.9.0">
+    <node TEXT="Inteligencia Artificial
+Agentes Inteligentes" FOLDED="false" POSITION="right" ID="5a81e37b1c5b5b0001a405f6" X_COGGLE_POSX="0" X_COGGLE_POSY="0">
+        <edge COLOR="#b4b4b4" />
+        <font NAME="Helvetica" SIZE="10" />
+        <node TEXT="Buen comportamiento" FOLDED="false" POSITION="right" ID="a720f0fa1ddcde602d9530de4efdccda">
+            <edge COLOR="#9ed56b" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Racionalidad" FOLDED="false" POSITION="right" ID="e22e0d1513bc07d858cacd3d4b149c5f">
+                <edge COLOR="#9fd668" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="aprendizaje" FOLDED="false" POSITION="right" ID="da3912d3bb7cdb9dea06568827238937">
+                    <edge COLOR="#a3d76b" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="exploración" FOLDED="false" POSITION="right" ID="cdaee455ba5930f4bcc4f3f20aefdd6a">
+                    <edge COLOR="#9cd55f" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="recolección de información" FOLDED="false" POSITION="right" ID="553d5432b152e334cf5f32d389a3499d">
+                    <edge COLOR="#a3d969" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="secuencia de percepción" FOLDED="false" POSITION="right" ID="e24fc96449215b7aa18555c4fba8a83e">
+                    <edge COLOR="#9cd361" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="acciones posibles" FOLDED="false" POSITION="right" ID="c5b743f32ede24b1a41a0113c4485ccd">
+                    <edge COLOR="#a1d46a" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Conocimiento del medio acumulado" FOLDED="false" POSITION="right" ID="eafeca2c765dd118b8cb8da4a4a99d9a">
+                    <edge COLOR="#a5da6c" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="medida: éxito" FOLDED="false" POSITION="right" ID="aa6d36d6f5a404507277fef687470dc6">
+                    <edge COLOR="#a3d86a" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT=" agente racional" FOLDED="false" POSITION="right" ID="925d4952df482ef6eaaa7be83680b4b4">
+                <edge COLOR="#a2d470" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT=" hace lo correcto en términos conceptuales" FOLDED="false" POSITION="right" ID="bc8f3f265e9172566ade47707abf5989">
+                    <edge COLOR="#a2d16f" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Medidas de rendimiento" FOLDED="false" POSITION="right" ID="bb80a91288cf7aa08888fc62c97bb729">
+                <edge COLOR="#a6d775" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="criterios que determinan el éxito en el comportamiento
+del agente" FOLDED="false" POSITION="right" ID="be6589ad07d16062319bef8a73e3f4bc">
+                    <edge COLOR="#a2d46c" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="Omnisciencia, 
+aprendizaje y 
+autonomía" FOLDED="false" POSITION="right" ID="5167e87def7899f723ed956732728e1f">
+                <edge COLOR="#a4d870" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT=" agente: omnisciente conoce el resultado de su acción y actúa de acuerdo con él" FOLDED="false" POSITION="right" ID="11d95b6f766d3e1ab250a2b3c600692e">
+                    <edge COLOR="#aad977" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+        <node TEXT="La naturaleza del entorno" FOLDED="false" POSITION="left" ID="a8b40523b7627bb06285462759b9b704">
+            <edge COLOR="#ebd95f" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="Estructura de los agentes" FOLDED="false" POSITION="left" ID="c0bbdbcfe7d17d1c63b88c98a2a722d5">
+                <edge COLOR="#ead556" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="agente= arquitectura + programa" FOLDED="false" POSITION="left" ID="717adc87ef312faf5c631f1d1672b526">
+                    <edge COLOR="#e8cf4e" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Programas de los agentes" FOLDED="false" POSITION="left" ID="05f7e860bd1984aba46d32862b9668ba">
+                    <edge COLOR="#e8d054" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="tipos básicos" FOLDED="false" POSITION="left" ID="c4acd4a499c7dab98d01b0d3c790787a">
+                        <edge COLOR="#e5ca4b" />
+                        <font NAME="Helvetica" SIZE="10" />
+                        <node TEXT="Agentes basados en utilidad" FOLDED="false" POSITION="left" ID="871321a56834e5bc7c264f71f81113a0">
+                            <edge COLOR="#e3c547" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                        <node TEXT="Agentes basados en objetivos" FOLDED="false" POSITION="left" ID="f1c3313bb531777e3b11d40f89f5a061">
+                            <edge COLOR="#e3c441" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                        <node TEXT="Agentes reactivos simples
+" FOLDED="false" POSITION="left" ID="c11b1a27c2c373a306dcfbc77919c05f">
+                            <edge COLOR="#e8c94a" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                        <node TEXT=" Agentes reactivos basados en modelos" FOLDED="false" POSITION="left" ID="2aadf0a38f125bfc505ab8cd1a07fb52">
+                            <edge COLOR="#e2c444" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                    </node>
+                    <node TEXT=" reciben sólo la percepción
+actual como entrada porque no hay nada más disponible en el entorno" FOLDED="false" POSITION="left" ID="2bc69e597dac329418c30d9efdf39b9b">
+                        <edge COLOR="#e6cc54" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="programas de los agentes,
+que toman la percepción actual como entrada, y la función del agente, que recibe la
+percepción histórica completa" FOLDED="false" POSITION="left" ID="80bb4eee1eb9927acb27fa499cb191df">
+                        <edge COLOR="#e8cf60" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+            </node>
+            <node TEXT=" agentes software" FOLDED="false" POSITION="left" ID="ffa49d412d1d12b03629ab98f5017814">
+                <edge COLOR="#ebd555" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="entorno de trabajo-&gt; problemas
+agentes-&gt;soluciones" FOLDED="false" POSITION="left" ID="82c73468244764c28d60e5c97e936202">
+                <edge COLOR="#ecd654" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="Especificación del entorno de trabajo" FOLDED="false" POSITION="left" ID="779d223c89a44780073634c41eb5e714">
+                <edge COLOR="#ebd75f" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="Rendimiento" FOLDED="false" POSITION="left" ID="79a01b0b724a127895fd8ccf7111121b">
+                    <edge COLOR="#ead460" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Entorno" FOLDED="false" POSITION="left" ID="045eb52299428ca257d0c42dc2f1078f">
+                    <edge COLOR="#ecd45a" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Actuadores" FOLDED="false" POSITION="left" ID="a8c5e88d71b993a6b6e87756080bb97d">
+                    <edge COLOR="#edd869" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Sensores" FOLDED="false" POSITION="left" ID="daa5cfcb0a64f7a53f3c4e1e9c8dc6e7">
+                    <edge COLOR="#ebd563" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="propiedades" FOLDED="false" POSITION="left" ID="f6a6a88440febe0cf7ac75f5f37c9880">
+                    <edge COLOR="#ebd45b" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="Agente individual vs. multiagente" FOLDED="false" POSITION="left" ID="295845536e24c5d81926d1f0b7e4c194">
+                        <edge COLOR="#e8cf5a" />
+                        <font NAME="Helvetica" SIZE="10" />
+                        <node TEXT="comunicativo" FOLDED="false" POSITION="left" ID="017b933fb0991bca506c05fe96e3da50">
+                            <edge COLOR="#e8cc56" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                        <node TEXT="cooperativo" FOLDED="false" POSITION="left" ID="91db54495f203353643e492bfe9020e7">
+                            <edge COLOR="#ebd163" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                        <node TEXT="competitivo" FOLDED="false" POSITION="left" ID="97a6955e1a8aa4023f80e5c83871344d">
+                            <edge COLOR="#ebd05f" />
+                            <font NAME="Helvetica" SIZE="10" />
+                        </node>
+                    </node>
+                    <node TEXT="Episódico vs. secuencial" FOLDED="false" POSITION="left" ID="be01710679f81d27eccafb20ad4c1a34">
+                        <edge COLOR="#ebd25f" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Totalmente observable vs. parcialmente observable" FOLDED="false" POSITION="left" ID="7746c5465e3197eb59b014403d43da5f">
+                        <edge COLOR="#edd461" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Determinista vs. estocástico." FOLDED="false" POSITION="left" ID="3759eb774cb0fe70173a1f913ae6ed07">
+                        <edge COLOR="#ebd25f" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Estático vs. dinámico" FOLDED="false" POSITION="left" ID="e2c36a1a4eb5f0148d5faa10c9b1b2ba">
+                        <edge COLOR="#ecd360" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="Discreto vs. continuo" FOLDED="false" POSITION="left" ID="a963d1cb350314c1133981cac1b474d4">
+                        <edge COLOR="#edd359" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+            </node>
+        </node>
+        <node TEXT="Agentes y su entorno
+" FOLDED="false" POSITION="right" ID="a9ef3cc1dec906cce87cd08f7ad1edb2">
+            <edge COLOR="#67d7c4" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="agente" FOLDED="false" POSITION="right" ID="bc5b22d364c1a1c2ce4b7938869080ab">
+                <edge COLOR="#67d5c0" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT=" cualquier cosa capaz de percibir su medioambiente" FOLDED="false" POSITION="right" ID="28ce5187eae75efa79f5626afd385410">
+                    <edge COLOR="#69d3bd" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="función" FOLDED="false" POSITION="right" ID="52b38d3afddac5de2cbbf995c79bc92c">
+                        <edge COLOR="#6cd4bd" />
+                        <font NAME="Helvetica" SIZE="10" />
+                        <node TEXT="comportamiento de un agente
+ se puede presentar en forma
+de tabla" FOLDED="false" POSITION="right" ID="3c32d30b31789a0b5aee3916e6ad7c05">
+                            <edge COLOR="#6ad2b9" />
+                            <font NAME="Helvetica" SIZE="10" />
+                            <node TEXT="programa del agente" FOLDED="false" POSITION="right" ID="3d216c52c764da1582f5972f90cdf981">
+                                <edge COLOR="#63cdb2" />
+                                <font NAME="Helvetica" SIZE="10" />
+                                <node TEXT=" implementación completa
+ de la función del agente" FOLDED="false" POSITION="left" ID="b5d950ffc099b750cbb9f9a0105b68c2">
+                                    <edge COLOR="#60ceb0" />
+                                    <font NAME="Helvetica" SIZE="10" />
+                                </node>
+                            </node>
+                        </node>
+                    </node>
+                    <node TEXT="Sensores" FOLDED="false" POSITION="right" ID="b7d45d5c8fa0b6105d4a069bc2824891">
+                        <edge COLOR="#6ed4bd" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="actuadores" FOLDED="false" POSITION="right" ID="ecc6b3f9a02e407458b9554dcfb7097a">
+                        <edge COLOR="#6ad0b9" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+                <node TEXT="Percepción" FOLDED="false" POSITION="right" ID="0c7f21f9f4d6e37f694fe0219433233a">
+                    <edge COLOR="#5fd3bb" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT=" el agente puede
+recibir entradas en cualquier instante" FOLDED="false" POSITION="right" ID="2895534259e65bf8ec8c50c8b79c73f4">
+                        <edge COLOR="#5fd1b7" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+            </node>
+        </node>
+        <node TEXT="naturaleza de los agentes ideales, sus diversos hábitats y las
+formas de organizar los tipos de agentes existentes." FOLDED="false" POSITION="left" ID="56315840ed24c91cf12e3e46d1cfbd34">
+            <edge COLOR="#7aa3e5" />
+            <font NAME="Helvetica" SIZE="10" />
+        </node>
+    </node>
+    <x-coggle-rootnode TEXT="Samantha Arburola 
+Ing. Computación
+Tecnológico de Costa Rica
+
+Inteligencia Artificial:
+Un Enfoque Moderno
+Russell, S. &amp; Norving, P." FOLDED="false" POSITION="right" ID="7427c19df8161bdc6db9b3536d02495c" X_COGGLE_POSX="-803.4137424857926" X_COGGLE_POSY="151.17087332185105">
+        <edge COLOR="#b4b4b4" />
+        <font NAME="Helvetica" SIZE="10" />
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="" FOLDED="false" POSITION="right" ID="87467ea7f2debe2ee381e713dc4bae34" X_COGGLE_POSX="451.81407082710535" X_COGGLE_POSY="314.23159061283644">
+        <edge COLOR="#b4b4b4" />
+        <font NAME="Helvetica" SIZE="10" />
+    </x-coggle-rootnode>
+</map>
---
+++ mapas mentales/4-Inteligencia Artificial Búsqueda informada y exploración.mm
@@ -0,0 +1,106 @@
+
+<map version="0.9.0">
+    <node TEXT="Inteligencia Artificial
+Búsqueda informada y
+exploración " FOLDED="false" POSITION="right" ID="5a81ff59218c150001cb54c2" X_COGGLE_POSX="0" X_COGGLE_POSY="0">
+        <edge COLOR="#b4b4b4" />
+        <font NAME="Helvetica" SIZE="10" />
+        <node TEXT=" búsqueda en línea" FOLDED="false" POSITION="left" ID="38f6605906da1342d78f301a860184f8">
+            <edge COLOR="#ebd95f" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT=" construir un mapa y encontrar
+un objetivo si existe" FOLDED="false" POSITION="left" ID="c3e16ab1048c9c0537710e2637ff9e4b">
+                <edge COLOR="#ebd765" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="BRPM y A*MS" FOLDED="false" POSITION="right" ID="f7eb8967f385cd590c9d4d9bde0a019e">
+            <edge COLOR="#988ee3" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT=" búsqueda robusta y óptima que utiliza cantidades
+limitadas de memoria" FOLDED="false" POSITION="right" ID="e4753e25a4268d5ea990b52867c9cfde">
+                <edge COLOR="#9f98e5" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="Búsqueda primero el mejor avara" FOLDED="false" POSITION="left" ID="f9360fd5166f1da6c663d6b55ba6f685">
+            <edge COLOR="#7aa3e5" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="a expande nodos con h(n) mínima" FOLDED="false" POSITION="left" ID="84f050861eebdd8423cca7881d4b3c3f">
+                <edge COLOR="#85ace6" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="heurísticas" FOLDED="false" POSITION="right" ID="f468f4cc8208fa553b70fcca41d83396">
+            <edge COLOR="#7aa3e5" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="optimalidad" FOLDED="false" POSITION="right" ID="9afcccfcde5f8adf55350067d943d16b">
+                <edge COLOR="#7da7e6" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT=" Búsqueda A*" FOLDED="false" POSITION="left" ID="0a71c6e50227273cb61757a4e620688f">
+            <edge COLOR="#7aa3e5" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="expande nodos con mínimo f(n) = g(n) + h(n)" FOLDED="false" POSITION="left" ID="1458a44247823d16d14cdef6db324bee">
+                <edge COLOR="#7aa5e5" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="problemas de exploración" FOLDED="false" POSITION="right" ID="7c17c6110949cee8a2e4f9f8770847da">
+            <edge COLOR="#9ed56b" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="cuando el agente no tiene la menor idea
+acerca de los estados y acciones de su entorno" FOLDED="false" POSITION="right" ID="632377bf6e5566d594e4dc144e1c29fb">
+                <edge COLOR="#9bd363" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT=" Las estimaciones de las heurística, actualizadas conla
+experiencia, proporcionan un método efectivo para escapar de mínimos locales. " FOLDED="false" POSITION="right" ID="e2d29d1948bdfeb17048731e8a82224b">
+                    <edge COLOR="#9ad45c" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+        <node TEXT="búsqueda primero el mejor" FOLDED="false" POSITION="left" ID="3feaf964f061e8f97c706eabd599783c">
+            <edge COLOR="#988ee3" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT=" los nodos no expandidos
+de costo mínimo (según alguna medida) se escogen para la expansión" FOLDED="false" POSITION="left" ID="7eb59aac0a7fd47b0a1f281db6445aab">
+                <edge COLOR="#938ae1" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT=" algoritmo genético" FOLDED="false" POSITION="right" ID="76e48f1ad61e45596a099d339614512b">
+            <edge COLOR="#5ed6c0" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT=" búsqueda de ascensión de colinas estocástica en la
+que se mantiene una población grande de estados" FOLDED="false" POSITION="right" ID="a966eeef5d9f52b087ab72eaf02a7b05">
+                <edge COLOR="#5ed4bc" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="ascensión de colinas" FOLDED="false" POSITION="left" ID="61375a9f56cf4a2f98750dba282d56d1">
+            <edge COLOR="#67d7c4" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="operan en formulaciones
+completas de estados" FOLDED="false" POSITION="left" ID="113ece4be9172660b15eb95bafe610fe">
+                <edge COLOR="#6ed8c4" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+    </node>
+    <x-coggle-rootnode TEXT="Samantha Arburola 
+Ing. Computación
+Tecnológico de Costa Rica
+
+Inteligencia Artificial:
+Un Enfoque Moderno
+Russell, S. &amp; Norving, P." FOLDED="false" POSITION="right" ID="aa8de94c4cb162c8bfb4484d67a30f68" X_COGGLE_POSX="-747.294448964988" X_COGGLE_POSY="297.6884948583846">
+        <edge COLOR="#b4b4b4" />
+        <font NAME="Helvetica" SIZE="10" />
+    </x-coggle-rootnode>
+    <x-coggle-rootnode TEXT="" FOLDED="false" POSITION="right" ID="cc85fab7f358073887e39dc692d685d8" X_COGGLE_POSX="740.6826664149406" X_COGGLE_POSY="6.054626700939569">
+        <edge COLOR="#b4b4b4" />
+        <font NAME="Helvetica" SIZE="10" />
+    </x-coggle-rootnode>
+</map>
