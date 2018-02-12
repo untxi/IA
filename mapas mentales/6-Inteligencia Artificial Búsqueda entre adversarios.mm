83967fc1c3b871bc1e2c75b38298740b987829a5
Sammy Arburola <samarburola@gmail.com>
2018-02-12 15:42:45 -0600

    mind maps

---
+++ mapas mentales/6-Inteligencia Artificial Búsqueda entre adversarios.mm
@@ -0,0 +1,137 @@
+
+<map version="0.9.0">
+    <node TEXT="Inteligencia Artificial
+Búsqueda entre adversarios" FOLDED="false" POSITION="right" ID="5a8207d3218c150001cbc4c8" X_COGGLE_POSX="0" X_COGGLE_POSY="0">
+        <edge COLOR="#b4b4b4" />
+        <font NAME="Helvetica" SIZE="10" />
+        <node TEXT="juegos al azar" FOLDED="false" POSITION="right" ID="1a83b04de84796d41441fcea31a61872">
+            <edge COLOR="#ebd95f" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="nodo de posibilidad" FOLDED="false" POSITION="right" ID="7fad4b7ffa5c2822300ff44a8f4d7200">
+                <edge COLOR="#ebd761" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="ponderación" FOLDED="false" POSITION="right" ID="b01492158df5a598833fadceac9ad5b5">
+                    <edge COLOR="#ecd355" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+        <node TEXT=" información imperfecta" FOLDED="false" POSITION="left" ID="751d774a7a3063804202d949edece20d">
+            <edge COLOR="#efa670" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="equiparse mejor para" FOLDED="false" POSITION="left" ID="1cdb3500f27512aba4d357e43e023505">
+                <edge COLOR="#efab7c" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="ajedrez" FOLDED="false" POSITION="left" ID="9f1878249eaad5c69fa47350f8c0c1c4">
+                    <edge COLOR="#f0a473" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="backgammon" FOLDED="false" POSITION="left" ID="abf0f55c1cefc7d23880863bfb36730d">
+                    <edge COLOR="#eea87b" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Damas" FOLDED="false" POSITION="left" ID="03c90de23d03347a541a58eb8df2085d">
+                    <edge COLOR="#f0ac80" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="Otelo" FOLDED="false" POSITION="left" ID="fd24d439eecfbb8d1b94111988f19c15">
+                    <edge COLOR="#eda87c" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT="bridge" FOLDED="false" POSITION="left" ID="8012e0a2cf8b9985b678b95c2e110ace">
+                <edge COLOR="#eea877" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT=" requiere" FOLDED="false" POSITION="left" ID="287f30beced58e510442cb8c833417eb">
+                <edge COLOR="#f0a775" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="el razonamiento sobre los estados de 
+creencia actuales y futuros de cada jugador" FOLDED="false" POSITION="left" ID="8faae234b131c3b4b4cea4cce52aa418">
+                    <edge COLOR="#f0a575" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+        <node TEXT="Teoría de Juegos" FOLDED="false" POSITION="right" ID="12f56148c02714cae6c6f3d09952b254">
+            <edge COLOR="#7aa3e5" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT=" rama de la Economía" FOLDED="false" POSITION="right" ID="2317ef0f682cdcd510c859858d216072">
+                <edge COLOR="#83abe8" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+            <node TEXT="componentes" FOLDED="false" POSITION="right" ID="765630a8914761f2f3c8ab82cbfa69ff">
+                <edge COLOR="#729ee1" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="función utilidad" FOLDED="false" POSITION="right" ID="7ac99f654f158533eca3350c5579ee80">
+                    <edge COLOR="#78a4e3" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="función sucesor" FOLDED="false" POSITION="right" ID="da300f7f564b3677114d1ada48b35b77">
+                    <edge COLOR="#6d9de2" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="test terminal" FOLDED="false" POSITION="right" ID="d56e6eb6667415b2854cb7773f3ea446">
+                    <edge COLOR="#719fe0" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+                <node TEXT="estado incial" FOLDED="false" POSITION="right" ID="8479d36d9cd1774a8989b5f0373926cc">
+                    <edge COLOR="#77a4e4" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+            <node TEXT=" juegos de suma cero" FOLDED="false" POSITION="right" ID="ddaf89a85795e4879beacab99c7ac531">
+                <edge COLOR="#7ba6e6" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT=" dos jugadores, por turnos, determinista,
+de información perfecta" FOLDED="false" POSITION="right" ID="feadda8425ce323aeca3d7dd80bba2f7">
+                    <edge COLOR="#7fabe8" />
+                    <font NAME="Helvetica" SIZE="10" />
+                    <node TEXT="minimo" FOLDED="false" POSITION="right" ID="fa828e046a70d009999bd6a5f724826f">
+                        <edge COLOR="#7aa9e5" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                    <node TEXT="máximo" FOLDED="false" POSITION="right" ID="6186ac50b8c326fd9a849e7085652daf">
+                        <edge COLOR="#73a6e8" />
+                        <font NAME="Helvetica" SIZE="10" />
+                    </node>
+                </node>
+            </node>
+            <node TEXT=" árbol de juegos" FOLDED="false" POSITION="right" ID="73d449debad3487a6c5d9142b1514fb8">
+                <edge COLOR="#7ca7e7" />
+                <font NAME="Helvetica" SIZE="10" />
+                <node TEXT="tic-tac-toe" FOLDED="false" POSITION="right" ID="9ffa143c01939b88a753eb55a3ef087e">
+                    <edge COLOR="#7ba9e8" />
+                    <font NAME="Helvetica" SIZE="10" />
+                </node>
+            </node>
+        </node>
+        <node TEXT="función
+de evaluación" FOLDED="false" POSITION="left" ID="7bbb348fadfdb3a87a6eb1f7eb268de2">
+            <edge COLOR="#9ed56b" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT="al acortar un árbol de juegos" FOLDED="false" POSITION="left" ID="bd9b41c61b1b043185345753c371c062">
+                <edge COLOR="#a3d76f" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+        <node TEXT="algoritmo e búsqueda alfa-beta" FOLDED="false" POSITION="right" ID="3f4458fda52d48f0d36b2c59fa70ceb5">
+            <edge COLOR="#67d7c4" />
+            <font NAME="Helvetica" SIZE="10" />
+            <node TEXT=" consigue una eficiencia mucho mayor al del MIN-MAX" FOLDED="false" POSITION="right" ID="c544a21b0ca36c02ba9ff4cf205f35fe">
+                <edge COLOR="#67d5c1" />
+                <font NAME="Helvetica" SIZE="10" />
+            </node>
+        </node>
+    </node>
+    <x-coggle-rootnode TEXT="Samantha Arburola 
+Ing. Computación
+Tecnológico de Costa Rica
+
+Inteligencia Artificial:
+Un Enfoque Moderno
+Russell, S. &amp; Norving, P." FOLDED="false" POSITION="right" ID="69027224a4df46d5afb28b810f46d11d" X_COGGLE_POSX="-532.4823617628022" X_COGGLE_POSY="254.19676570830273">
+        <edge COLOR="#b4b4b4" />
+        <font NAME="Helvetica" SIZE="10" />
+    </x-coggle-rootnode>
+</map>
