2da164c16ad5ceb3359fad98b74a03c62ad16dd6
Sammy Arburola <samarburola@gmail.com>
2018-03-03 09:43:10 -0600

    tarea n-reinas

---
+++ codigo/n-reinas.py
@@ -0,0 +1,32 @@
+n = 8
+array = []
+matrizPromedio = []
+
+def llenarMatriz(n):
+    for x in range(n):
+        for y in range(n):
+            matrizPromedio.append([0,0])
+    
+global reinaActual
+
+def calcularPromedio()
+    # El promedio se calcula sumando las fichas ya puestas divididas
+    # entre la ficha actual -1
+
+    # Fichas actuales: sumas de las heuristicas de las fichas anteriores
+    promedio = fichasActuales / (reinaActual - 1)
+
+
+
+def calcularAcumulado()
+
+
+def calcularHeuristica()
+
+
+def imprimirSoluciones()
+
+def main():
+    # Pedir n y llenar la matriz de ceros
+    n = int(input("Por favor ingrese un numero para la matriz: "))
+    llenarMatriz(n)
