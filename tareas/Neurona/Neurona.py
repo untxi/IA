5761648c7fbe23ee68cff5a3697c792174eaed10
Sammy Arburola <samarburola@gmail.com>
2018-05-01 14:19:10 -0600

    homework

---
+++ tareas/Neurona/Neurona.py
@@ -0,0 +1,56 @@
+# Tecnologico de Costa Rica
+# Escuela de Ing en Computacion
+# Curso de Inteligencia Artificial
+# Neurona
+# Problema de Categorizacion, Redes Neuronales
+# Samantha Arburola, Mayo 2018
+
+#Library
+import os
+import os.path
+
+# Auxiliar Function
+def FileExist(filePath):
+    if os.path.isfile(filePath) and os.access(filePath, os.R_OK):
+        return True
+    else:
+        return False
+
+
+class Neuron:
+    def __init__(self):
+        self.realDataC1 = []
+        self.realDataC2 = []
+        self.realData = self.realDataC1 + self.realDataC2
+        return True
+    
+    def getRealData(fileName, setBelongs):
+        """ Read a .tdata file """
+        try:
+            # print "File exists and is readable"
+            FileExist(fileName)
+        except:
+            print "Either file is missing or is not readable"
+            return False
+        
+        myFile = open(fileName, 'r')
+        myFile.readlines()
+
+        for line in myFile:
+            line = line.replace('\n', ' ')
+            line.split(' ');
+            for point in line:
+                setBelongs.append(point)
+
+        return True
+    
+
+    def Training(self):
+        return True
+
+        
+    def Classify(self):
+        return True
+           
+
+
